e97b45de3506c8c03fde25edeb6473f05d8cebd1
Seongho-Shin <sim2308@naver.com>
2022-11-21 16:44:08 +0900

    init

---
+++ begin-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './Hello';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ begin-react/src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react'; // 리액트 call
+
+function Hello() {
+    return <div>안녕하세요</div>
+}
+
+export default Hello; // Hello 컴포넌트를 내보내겠다는 뜻
