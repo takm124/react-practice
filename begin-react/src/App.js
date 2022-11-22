acb3ae348a013930e7fff8fecd1d48ae1200260f
Seongho-Shin <sim2308@naver.com>
2022-11-22 16:23:07 +0900

    react practice 1-3 ~ 1-13

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,114 @@
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
 import './App.css';
-import './Hello';
+import Wrapper from './Wrapper';
 import Hello from './Hello';
+import Counter from './Counter';
+import InputSample from './InputSample';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
   return (
-    <div className="App">
-      <Hello />
-    </div>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users = {users}/>
+    </>
+  );
+}
+
+function App_Input() {
+  return(
+    <InputSample />
+  );
+}
+
+function App_Counter() {
+  return (
+    <Counter />
+  );
+}
+
+
+function App_props() {
+  return (
+    <Wrapper>
+      <Hello name = "react" color = "red" isSpecial /> {/* isSpecial -> true면 생략 가능 */}
+      <Hello color = "pink" />
+    </Wrapper>
+  );
+}
+
+
+function App_JSX() {
+  const name = 'react';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24, // 기본단위 px
+    padding: '1rem' // 다른 단위 사용시 문자열로 설정
+  }
+  
+  return (
+    <> {/* 주석은 이런식으로 */}
+      <Hello 
+        //열리는 태그 내부에서는 주석 이렇게
+      />
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
+    </> 
   );
 }
 
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,17 @@
 import React from 'react'; // 리액트 call
 
-function Hello() {
-    return <div>안녕하세요</div>
+function Hello({color, name, isSpecial}) {
+    return (
+        <div style={{color}}>
+        {/* { isSpecial ? <b>*</b> : null} */}
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}</div>
+    );
+    
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
 }
 
 export default Hello; // Hello 컴포넌트를 내보내겠다는 뜻
