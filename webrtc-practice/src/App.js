a0f5ca9a928d6b7f9dfb9a8e31669d88212f36b7
Seongho-Shin <sim2308@naver.com>
2022-12-09 13:53:54 +0900

    peer connection

--- webrtc-practice/src/App.js
+++ webrtc-practice/src/App.js
@@ -56,18 +56,56 @@ function App() {
     })
   }
 
-  async function sendVideo() {
-    const constraints = { 'video': true };
-    stream = await navigator.mediaDevices.getUserMedia(constraints);
+  // peer connection
+  // Set up an asynchronous communication channel that will be
+  // used during the peer connection setup
+  const signalingChannel = new SignalingChannel(remoteClientId);
+  signalingChannel.addEventListener('message', message => {
+      // New message from remote client received
+  });
 
-    client.publish({
-      destination : '/app/video',
-      body : stream
-    })
+  // Send an asynchronous message to the remote client
+  signalingChannel.send('Hello!');
+
+  async function makeCall() {
+    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+    const peerConnection = new RTCPeerConnection(configuration);
+    signalingChannel.addEventListener('message', async message => {
+        if (message.answer) {
+            const remoteDesc = new RTCSessionDescription(message.answer);
+            await peerConnection.setRemoteDescription(remoteDesc);
+        }
+    });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    signalingChannel.send({'offer': offer});
+};
+
+// Listen for local ICE candidates on the local RTCPeerConnection
+peerConnection.addEventListener('icecandidate', event => {
+  if (event.candidate) {
+      signalingChannel.send({'new-ice-candidate': event.candidate});
   }
+});
 
- sendVideo();
-  
+// Listen for remote ICE candidates and add them to the local RTCPeerConnection
+signalingChannel.addEventListener('message', async message => {
+  if (message.iceCandidate) {
+      try {
+          await peerConnection.addIceCandidate(message.iceCandidate);
+      } catch (e) {
+          console.error('Error adding received ice candidate', e);
+      }
+  }
+});
+
+// Listen for connectionstatechange on the local RTCPeerConnection
+peerConnection.addEventListener('connectionstatechange', event => {
+  if (peerConnection.connectionState === 'connected') {
+      // Peers connected!
+  }
+});
+
 
   // Video 구현부
   var stream;
